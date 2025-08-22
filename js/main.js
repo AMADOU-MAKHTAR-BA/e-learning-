a1ca6c2d78652dcf386c927ec4cc41a8f6df8b62
AMADOU-MAKHTAR-BA <developeur123@gmail.com>
2025-08-22 00:10:39 +0000

    NAV_BAR UPDATE

--- js/main.js
+++ js/main.js
@@ -1,1 +1,15 @@
-console.log('Hello World!');
+const classe = document.querySelector("#conteneur");
+const statut = document.querySelector(".statut");
+
+function afficher_classes() {
+  classe.classList.toggle("afficher_classe");
+  
+  if (classe.classList.contains("afficher_classe")) {
+    classe.style.display = "block"
+  }
+  else {
+    classe.style.display = "none"
+  }
+}
+
+statut.onclick = afficher_classes
